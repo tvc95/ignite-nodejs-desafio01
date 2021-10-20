863becc97b564934298d38353a788df2ddc8da87
tvc95 <thiagovc1500@gmail.com>
2021-10-20 15:13:11 -0300

    Middleware function implemented; POST /routes and GET /todos implemented

---
+++ README.md
@@ -0,0 +1,15 @@
+# Desafio 01 - Rocketseat
+
+## Requisitos:
+- [] Criar um novo todo;
+- [X] Listar todos os todos;
+- [] Alterar o `title` e o `deadline` de um todo existente;
+- [] Marcar um todo como feito;
+- [] Excluir um todo;
+
+OBS.: todas essas funcionalidades para um usuário em específico. (O `username` será passado pelo header).
+
+## Outros requisitos
+- [X] Criar um usuário
+
+https://www.notion.so/Desafio-01-Conceitos-do-Node-js-59ccb235aecd43a6a06bf09a24e7ede8
--- src/index.js
+++ src/index.js
@@ -1,25 +1,66 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((u) => u.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
+/**
+ * A rota deve receber name, e username dentro do corpo da 
+ * requisição. Ao cadastrar um novo usuário, ele deve ser 
+ * armazenado dentro de um objeto no seguinte formato:
+ * { id (uuid), name (string), username (string), todos ([])}
+ * 
+ * Certifique-se que o ID seja um UUID, e de sempre iniciar a 
+ * lista todos como um array vazio. O objeto do usuário deve 
+ * ser retornado na resposta da requisição.
+ */
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userExists = users.some((user) => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({
+      error: 'Username already exists!'
+    });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
